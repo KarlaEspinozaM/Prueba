c9f02f0f864eee0218d881d8209aacb9d3d1e86c
Karla Espinoza <Karlita-1611@hotmail.com>
2024-07-31 11:34:19 -0500

    lib

--- src/module/medical-appointment/medical-appointment.module.ts
+++ src/module/medical-appointment/medical-appointment.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MedicalAppointmentController } from './medical-appointment.controller';
 import { MedicalAppointmentService } from './medical-appointment.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { medicalAppointment } from 'src/module/medical-appointment/entity/MedicalAppointment.entity';
 import { ClientModule } from '../client/client.module';
 
